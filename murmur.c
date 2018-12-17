b3c31637a9be2f437f1c9e853478d8eda1831e11
shreshtha shrivastava <shreshtha441@gmail.com>
2018-12-17 16:59:18 +0530

    Update murmur.c

--- murmur.c
+++ murmur.c
@@ -1,17 +1,13 @@
-#include<stdio.h>
-#include<stdint.h>
-#include<stdlib.h>
-#include<stdio.h>
 
-uint32_t murmur3_32(const uint32_t* key, size_t len, uint32_t seed)
+uint32_t murmur3_32(const uint64_t* key, size_t len, uint32_t seed)
 {
 uint32_t h1=seed;
 if(len>3)
 {
-const uint32_t* key_x4= (const uint32_t*) key;
+const uint64_t* key_x4= (const uint64_t*) key;
 size_t i=len>>2;
 do{
-uint32_t k1=*key_x4++;
+uint64_t k1=*key_x4++;
 k1*=0xcc9e2d51;
 k1=(k1<<15)|(k1>>17);
 k1*=0x1b873593;
@@ -20,12 +16,12 @@ h1=(h1<<13)|(h1>>19);
 h1=(h1*5)+0xe6546b64;
 }
 while(--i);
-key=(const uint32_t*) key_x4;
+key=(const uint64_t*) key_x4;
 }
 if(len&3)
 {
 size_t i=len&3;
-uint32_t k1=0;
+uint64_t k1=0;
 key=&key[i-1];
 do{
 k1 <<=8;
@@ -46,13 +42,5 @@ h1^=h1>>16;
 return h1;
 }
 
-int main()
-{
-uint32_t seed=99;
-size_t len=12;
-uint32_t p= 857394758392;
-const uint32_t* key= &p;
-uint32_t h1=murmur3_32(key,len,seed);
-printf("The hash value of 857394758392 is %hhu ",h1);
-return 0;
-}
+
+
